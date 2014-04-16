51add01b615705cc2205adaa8a582bd9569e780a
marcoau <marco.au.321@gmail.com>
2014-04-16 13:21:17 -0700

    Implemented Functional Instantiation style.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size++] = value;
   };
 
   instance.dequeue = function(){
+    if(size > 0){
+      var result = storage[0];
+      for(var i = 0; i < size; i++){
+        storage[0] = storage[1];
+      }
+      delete storage[--size];
+      return result;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
